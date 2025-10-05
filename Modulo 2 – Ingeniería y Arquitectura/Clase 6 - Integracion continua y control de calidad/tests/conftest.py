56110a1bb09ca065fa095a4bcec4eb7dde5feb54
Jarko <jarkoarenal@gmail.com>
2025-10-05 16:49:55 +0200

    Cambiado parents

--- Modulo 2 – Ingeniería y Arquitectura/Clase 6 - Integracion continua y control de calidad/tests/conftest.py
+++ Modulo 2 – Ingeniería y Arquitectura/Clase 6 - Integracion continua y control de calidad/tests/conftest.py
@@ -1,5 +1,5 @@
 import sys
 from pathlib import Path
 
-raiz_proyecto = Path(__file__).resolve().parents[1]
+raiz_proyecto = Path(__file__).resolve().parents[2]
 sys.path.insert(0, str(raiz_proyecto))
--- Modulo 2 – Ingeniería y Arquitectura/Clase 6 - Integracion continua y control de calidad/tests_integrations/conftest.py
+++ Modulo 2 – Ingeniería y Arquitectura/Clase 6 - Integracion continua y control de calidad/tests_integrations/conftest.py
@@ -3,5 +3,5 @@ import sys
 from pathlib import Path
 
 # Subimos dos niveles desde esta carpeta hasta la raíz del proyecto
-raiz_proyecto = Path(__file__).resolve().parents[1]
+raiz_proyecto = Path(__file__).resolve().parents[2]
 sys.path.insert(0, str(raiz_proyecto))
